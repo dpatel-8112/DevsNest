f28a58f58e9efb444e82297b66a7a0569fb9cdbe
Devarshi Patel <amazingdotdp@gmail.com>
2021-07-17 11:15:15 +0530

    Devsnest Frontend till Day-12

---
+++ Frontend/Day-12/scripts.js
@@ -0,0 +1,18 @@
+function addTask() {
+  let task = document.getElementById("task").value;
+
+  if (!task) return;
+
+  const text =
+    "<li class='task list-group-item list-group-item-danger mb-1 '>" +
+    task +
+    "</li>";
+  document.getElementById("list").insertAdjacentHTML("beforeend", text);
+  document.getElementById("task").value = "";
+}
+
+document.getElementById("add").addEventListener("click", addTask);
+document.getElementById("list").addEventListener("click", function (event) {
+  const element = event.target;
+  element.classList.toggle("done");
+});
